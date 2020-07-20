50f84ed4aa2570dd19eb174f8fd34beb1ace7f6e
Jason Dippel <dippelj12@gmail.com>
2020-07-20 11:42:14 -0700

    able to mark point on map

--- src/Map/index.js
+++ src/Map/index.js
@@ -4,9 +4,20 @@ import {
   Geographies,
   Geography,
   ZoomableGroup,
+  Marker,
 } from 'react-simple-maps'
 import geoData from './data/world-110m.json'
 
+const markers = [
+  {
+    markerOffset: -30,
+    name: 'Vancouver, BC',
+    coordinates: [-123.1207, 49.2827], // E(+)/W(-), N(+)/S(-)
+  },
+]
+
+// Focus points...
+// Canada: { rotate: [110, -65, 0], scale: 700 }
 export const Map = () => (
   <ComposableMap projectionConfig={{ scale: 200 }}>
     <ZoomableGroup zoom={1}>
@@ -20,20 +31,45 @@ export const Map = () => (
                 default: {
                   fill: '#D6D6DA',
                   outline: 'none',
+                  stroke: '#CCC',
                 },
                 hover: {
                   fill: '#F53',
                   outline: 'none',
+                  stroke: '#CCC',
                 },
                 pressed: {
                   fill: '#E42',
                   outline: 'none',
+                  stroke: '#CCC',
                 },
               }}
             />
           ))
         }
       </Geographies>
+      {markers.map(({ name, coordinates, markerOffset }) => (
+        <Marker key={name} coordinates={coordinates}>
+          <g
+            fill="none"
+            stroke="#000"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            transform="translate(-12, -24)"
+          >
+            <circle cx="12" cy="10" r="3" />
+            <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+          </g>
+          <text
+            textAnchor="middle"
+            y={markerOffset}
+            style={{ fontFamily: 'system-ui', fill: '#5D5A6D' }}
+          >
+            {name}
+          </text>
+        </Marker>
+      ))}
     </ZoomableGroup>
   </ComposableMap>
 )
